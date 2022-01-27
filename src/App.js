009154d97c1e0a8c5d296d2c614081b20c8c1578
tacrew <t.murakami411@gmail.com>
2022-01-27 23:27:25 +0900

    add connect wallet feat

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 // import { ethers } from "ethers";
 import './App.css';
 
 const App = () => {
-  const checkIfWalletIsConnected = () => {
-    /*
-    * First make sure we have access to window.ethereum
-    */
-    const { ethereum } = window;
+  const [currentAccount, setCurrentAccount] = useState('')
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({method: 'eth_accounts'})
+
+      if (!accounts.length) {
+        console.log('No authorized account found')
+        return
+      }
+
+      const account = accounts[0]
+      console.log(`Found an authorized account: ${account}`)
+      setCurrentAccount(account)
+
+    } catch (error) {
+      console.log(error)
     }
   }
+
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window
+
+      if(!ethereum) {
+        alert('Get Metamask!')
+        return
+      }
+
+      const accounts = await ethereum.request({method: 'eth_requestAccounts'})
+
+      console.log(`Connected ${accounts[0]}`)
+      setCurrentAccount(accounts[0])
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -34,6 +69,10 @@ const App = () => {
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>Connect Wallet</button>
+        )}
       </div>
     </div>
   );

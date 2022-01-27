de87740799b324c8a12188d91a7a9ad541a8d90d
tacrew <t.murakami411@gmail.com>
2022-01-27 23:48:45 +0900

    add wave wright feat

--- src/App.js
+++ src/App.js
@@ -68,6 +68,15 @@ const App = () => {
 
       let count = await wavePortalContract.getTotalWaves()
       console.log(`Retrieved total wave count...${count.toNumber()}`)
+
+      const waveTxn = await wavePortalContract.wave()
+      console.log(`Mining... ${waveTxn.hash}`)
+
+      await waveTxn.wait()
+      console.log(`Mined -- ${waveTxn.hash}`)
+
+      count = await wavePortalContract.getTotalWaves()
+      console.log(`Retrieved total wave count...${count.toNumber()}`)
     } catch(error) {
       console.log(error)
     }

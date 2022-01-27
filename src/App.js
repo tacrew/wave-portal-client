bf8c52c28a88d1b0ee26a483d074de371aee78c9
tacrew <t.murakami411@gmail.com>
2022-01-27 23:18:18 +0900

    mod app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import * as React from "react";
+import React, {useEffect} from "react";
 // import { ethers } from "ethers";
 import './App.css';
 
-export default function App() {
+const App = () => {
+  const checkIfWalletIsConnected = () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+    const { ethereum } = window;
 
-  const wave = () => {
-    
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
   }
-  
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
-        <span role="img" aria-label="wave">👋</span> Hey there!
+        <span role='img' aria-label="hi">👋</span> Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
     </div>
   );
 }
+
+export default App

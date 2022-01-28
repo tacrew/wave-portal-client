16a1552aa2f630be5de4d6d164a035c07c8f639a
tacrew <t.murakami411@gmail.com>
2022-01-28 19:48:56 +0900

    change output.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "../dist/output.css";
+import "./output.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
